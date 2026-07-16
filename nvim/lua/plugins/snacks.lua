32131a18366f5955c819332608cc97d334f73682
isaiah76 <fdscssdccefb@gmail.com>
2026-07-16 23:34:39 +0800

    nvim update

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts.picker = opts.picker or {}
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.explorer = vim.tbl_deep_extend("force", opts.picker.sources.explorer or {}, {
+        hidden = true,
+        ignored = true,
+      })
+    end,
+  },
+}
